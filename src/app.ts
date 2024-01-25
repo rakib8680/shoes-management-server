cdb86c13dd0c7b96b39e5dd87d479dd39997381d
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-25 16:06:28 +0600

    folder structure and authentication routes initialize

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
-import { userRoute } from './app/modules/user/user.routes';
+import router from './app/routes';
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
 // application route
-app.use('/api/users', userRoute);
+app.use('/api/users', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
