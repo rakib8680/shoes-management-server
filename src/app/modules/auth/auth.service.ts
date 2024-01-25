dcfdf8aedca72b8105c29e98bcd6fa8f6f783135
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-25 19:16:31 +0600

    Add login-user functionality

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,8 @@ import AppError from '../../errors/appError';
 import { TUser } from './auth.interface';
 import { User } from './auth.model';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 
 // register user
 const registerUser = async (payload: TUser) => {
@@ -11,6 +13,29 @@ const registerUser = async (payload: TUser) => {
 };
 
 
+
+
+// login user
+const loginUser = async (payload: TUser) => {
+  // check if the user exists
+  const user = await User.findOne({ email: payload.email });
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'user not found');
+  }
+
+  // check if the password is correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload.password,
+    user.password,
+  );
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'incorrect password');
+  }
+
+
+
+
+};
 
 export const AuthServices = {
   registerUser,
