1f93649aa15538714784b615fd287c58a2066810
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-26 11:58:15 +0600

    add create-shoes routes

--- src/app/modules/products/product.routes.ts
+++ src/app/modules/products/product.routes.ts
@@ -1,3 +1,13 @@
+import { Router } from "express";
+import { productControllers } from "./product.controller";
 
 
 
+const router = Router();
+
+
+router.post('/add-shoes', productControllers.addShoes);
+
+
+
+export const ProductRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthRoutes } from '../modules/auth/auth.routes';
+import { ProductRoutes } from '../modules/products/product.routes';
 
 const router = Router();
 
@@ -7,6 +8,10 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/products',
+    route: ProductRoutes,
   },
 ];
 
